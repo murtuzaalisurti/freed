cde855cfd31d320e2fbb057d42ae0aac7296797d
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-05-19 13:14:22 +0530

    chore: better comments

--- src/lib/api/fetchFeeds.ts
+++ src/lib/api/fetchFeeds.ts
@@ -1,10 +1,42 @@
 import FeedList from "../../data/feedlist.json" assert { type: 'json' }
 
+/**
+ * @explain
+ * It takes in the list `{ id: string, url: string }[]` array and then
+ * creates a new Set object instance, iterates over the list,
+ * compares the current size of the Set with the size after adding a new element.
+ * 
+ * If the element is a duplicate, the Set won't add it to itself and the size won't change,
+ * so, the previous size and the new size after adding that element will remain same.
+ * The condition would then be true and we would come to know if there is a duplicate or not
+ * using the `some` method in javascript.
+ * @param list - An array of { id, url }
+ * @param prop - Property to be checked - either `id` or `url`
+ * @returns { boolean } true/false depending on whether the list contains some dupes or not.
+ */
 const hasDuplicate = (list: Record<string, any>[], prop: string): boolean => {
     let uniqueSet = new Set()
     return list.some((obj) => uniqueSet.size === uniqueSet.add(obj[prop]).size)
 }
 
+/**
+ * @explain
+ * It takes in the list `{ id: string, url: string }[]` array and then
+ * maps it to an array containing just the passed property `(example: id => { id: string }[])` and then
+ * finds the duplicate element by comparing the index of the curr element with the index of the first occurrence of that element,
+ * if the index of the first occurence of the element is different than the index of the current element, then
+ * it means the element has existed before in the array
+ * 
+ * For example: in the array `[3,12,9]` if you add 12 again, then the first occurence index will be 1 and not 3
+ * a filter is used to do this comparision as we want to get all duplicate values
+ * there can be multiple duplicate values returned from the filter
+ * for example, `[33,33,67,23]` => all of these are duplicate values returned by the filter
+ * we can pass it into a Set to just get the `"unique duplicate"` values.
+ * 
+ * @param list - An array of { id, url }
+ * @param prop - Property to be checked - either `id` or `url`
+ * @returns An array containing *unique duplicate* values
+ */
 const getDuplicate = (list: Record<string, any>[], prop: string): string[] => {
     return [
         ...new Set(list.map(fl => fl[prop]).filter((f, i, arr) => arr.indexOf(f) !== i))
