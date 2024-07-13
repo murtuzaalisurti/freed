0201cb47caf83112e9097adb3d0e00036bb7afd3
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-07-13 14:57:27 +0530

    refactor: adFeed script

--- scripts/addFeed.ts
+++ scripts/addFeed.ts
@@ -4,16 +4,34 @@ import prompts, { type PromptObject } from 'prompts'
 import { logger } from '../src/lib/logger.ts'
 import { validateFeeds } from '../src/lib/api/fetchFeeds.ts'
 
-const feedlist: { id: string, url: string }[] = JSON.parse(
-    fs.readFileSync(new URL("../src/data/feedlist.json", import.meta.url)) as unknown as string
-);
-
-const updatedFeeds = (feeds: { id: string, url: string }[], urls: string[]) => {
-    return [
-        ...feeds,
-        ...urls.map(url => ({ id: v7(), url }))
-    ]
+interface Feed {
+    id: string
+    url: string
 }
+
+const readFeedList = () => {
+    const feedlist: Feed[] = JSON.parse(
+        fs.readFileSync(new URL("../src/data/feedlist.json", import.meta.url)) as unknown as string
+    );
+    return feedlist;
+}
+
+const writeUpdatedFeedList = (updatedFeedList: Feed[]) => {
+    fs.writeFileSync(
+        "./src/data/feedlist.json",
+        JSON.stringify(
+            updatedFeedList,
+            null,
+            2
+        ),
+        {
+            encoding: 'utf8'
+        }
+    )
+}
+
+const updatedFeeds = (feeds: Feed[], urls: string[]) =>
+    feeds.concat(urls.map(url => ({ id: v7(), url })));
 
 async function ask() {
     const askPrompts: PromptObject<string>[] = [
@@ -29,22 +47,11 @@ async function ask() {
     return await prompts(askPrompts, { onCancel })
 }
 
+const feedlist = readFeedList()
 const { url } = await ask()
-
 const updatedFeedList = updatedFeeds(feedlist, url)
 
 validateFeeds(updatedFeedList)
-
-fs.writeFileSync(
-    "./src/data/feedlist.json",
-    JSON.stringify(
-        updatedFeedList,
-        null,
-        2
-    ),
-    {
-        encoding: 'utf8'
-    }
-)
+writeUpdatedFeedList(updatedFeedList)
 
 logger.success(`Added feed(s): \n\t- ${url.join("\n\t- ")}`)
