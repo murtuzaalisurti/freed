dcb00435057d49f7d5729f4f056e377eabd1e51f
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-08-03 11:37:58 +0530

    fix: undefined exception

--- src/lib/utils/dates.ts
+++ src/lib/utils/dates.ts
@@ -2,8 +2,8 @@ import { FeedDataType, type DateReturnType, type Feed, type FeedItem } from "./t
 
 export const getDates = (data: Feed | FeedItem, type: FeedDataType): DateReturnType => {
     if (type === FeedDataType.FEED) {
-        const publishDate = data.items[0].pubDate || data.items[0].isoDate || null
-        const lastBuildDate = data.lastBuildDate || data.items[0].pubDate || data.items[0].isoDate || null
+        const publishDate = data.items[0]?.pubDate || data.items[0]?.isoDate || null
+        const lastBuildDate = data.lastBuildDate || data.items[0]?.pubDate || data.items[0]?.isoDate || null
         return {
             publishDate,
             lastBuildDate,
@@ -12,7 +12,7 @@ export const getDates = (data: Feed | FeedItem, type: FeedDataType): DateReturnT
     }
     if (type === FeedDataType.FEED_ITEM) {
         return {
-            publishDate: data.pubDate || data.isoDate || null
+            publishDate: data?.pubDate || data?.isoDate || null
         }
     }
     return {
